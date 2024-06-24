320673efaf3441063a36bbb1db14ae5490d5c7af
Vasil Kekelia <tskapodevs@icloud.com>
2024-06-24 10:48:44 +0200

    მორგება, ახალი თემის დამატება

--- init.lua
+++ init.lua
@@ -225,6 +225,41 @@ vim.opt.rtp:prepend(lazypath)
 --
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
+	{ -- You can easily change to a different colorscheme.
+    -- Change the name of the colorscheme plugin below, and then
+    -- change the command in the config to whatever the name of that colorscheme is.
+    --
+    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+    'navarasu/onedark.nvim',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    init = function()
+      -- Load the colorscheme here.
+      -- Like many other themes, this one has different styles, and you could load
+      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      vim.cmd.colorscheme 'onedark'
+
+      -- You can configure highlights by doing something like:
+      vim.cmd.hi 'Comment gui=none'
+    end,
+   },
+  --'rmehri01/onenord.nvim',
+  {
+    'glepnir/nerdicons.nvim',
+    cmd = 'NerdIcons',
+    config = function()
+      require('nerdicons').setup {}
+    end,
+  },
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v3.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+      'MunifTanjim/nui.nvim',
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+  },
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
@@ -772,24 +807,23 @@ require('lazy').setup({
       }
     end,
   },
-
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
-
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
-    end,
-  },
+--   { -- You can easily change to a different colorscheme.
+--     -- Change the name of the colorscheme plugin below, and then
+--     -- change the command in the config to whatever the name of that colorscheme is.
+--     --
+--     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+--     'folke/tokyonight.nvim',
+--     priority = 1000, -- Make sure to load this before all the other start plugins.
+--     init = function()
+--       -- Load the colorscheme here.
+--       -- Like many other themes, this one has different styles, and you could load
+--       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+--       vim.cmd.colorscheme 'tokyonight-night'
+-- 
+--       -- You can configure highlights by doing something like:
+--       vim.cmd.hi 'Comment gui=none'
+--     end,
+--   },
 
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
